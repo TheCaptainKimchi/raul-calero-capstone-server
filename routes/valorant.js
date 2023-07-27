18addfb309f58cbaa2a539ca3e507c8464de149d
Raul Calero <raulcalero7@gmail.com>
2023-07-27 10:36:47 -0700

    Added functionality to GET /leaderboard so that when it is called, will iterate through the data stored in leaderboard.json and return the player with the most kills, the most assists, the most deaths, and the best overall KDA

--- routes/valorant.js
+++ routes/valorant.js
@@ -8,6 +8,7 @@ const path = require("path");
 
 const apiKey = process.env.API_KEY;
 const cache = {};
+const filePath = path.join(__dirname, "..", "data", "leaderboard.json");
 
 router.route("/").get((req, res) => {
   console.log(apiKey);
@@ -73,53 +74,209 @@ router.route("/match").get((req, res) => {
     });
 });
 
-router.route("/leaderboard").post((req, res) => {
-  const filePath = path.join(__dirname, "..", "data", "leaderboard.json");
-  const obj = {
-    id: req.query.id,
-    userName: req.query.userName,
-    tagline: req.query.tagline,
-    puuid: req.query.puuid,
-    kills: req.query.kills,
-    deaths: req.query.deaths,
-    assists: req.query.assists,
-    kda: req.query.kda,
-    acs: req.query.acs,
-    map: req.query.map,
-    agent: req.query.agent,
-    mode: req.query.mode,
-  };
+router
+  .route("/leaderboard")
+  .post((req, res) => {
+    const obj = {
+      id: req.query.id,
+      userName: req.query.userName,
+      tagline: req.query.tagline,
+      puuid: req.query.puuid,
+      kills: req.query.kills,
+      deaths: req.query.deaths,
+      assists: req.query.assists,
+      kda: req.query.kda,
+      acs: req.query.acs,
+      map: req.query.map,
+      agent: req.query.agent,
+      mode: req.query.mode,
+    };
 
-  try {
-    // Read the existing data from the JSON file (if any)
-    let existingData = [];
-    if (fs.existsSync(filePath)) {
-      const dataString = fs.readFileSync(filePath, "utf8");
-      existingData = JSON.parse(dataString);
-    }
+    try {
+      // Read the existing data from the JSON file (if any)
+      let existingData = [];
+      if (fs.existsSync(filePath)) {
+        const dataString = fs.readFileSync(filePath, "utf8");
+        existingData = JSON.parse(dataString);
+      }
 
-    // Check if data with the given id already exists
-    if (isDataExists(existingData, obj)) {
-      return res
+      // Check if data with the given id already exists
+      if (isDataExists(existingData, obj)) {
+        return res
+          .status(200)
+          .json({ Response: "Data already added to database." });
+      }
+
+      // Append the received data to the existing data
+      existingData.push(obj);
+
+      // Write the updated data back to the JSON file
+      fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2), "utf8");
+
+      // Respond with a success message
+      res
         .status(200)
-        .json({ Response: "Data already added to database." });
+        .json({ message: "Data added to the JSON file successfully." });
+    } catch (error) {
+      console.error("Error processing the request:", error);
+      res.status(400).json({ error: "Something went wrong." });
     }
+  })
+  .get((req, res) => {
+    // Read the contents of the leaderboard.json file
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the leaderboard.json file:", err);
+        return;
+      }
 
-    // Append the received data to the existing data
-    existingData.push(obj);
+      // Parse the JSON data into an array of objects
+      const leaderboardData = JSON.parse(data);
 
-    // Write the updated data back to the JSON file
-    fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2), "utf8");
+      // Create an object to store the total kills and names for each unique puuid
+      const puuidKillsMap = {};
+      const puuidDeathsMap = {};
+      const puuidAssistsMap = {};
 
-    // Respond with a success message
-    res
-      .status(200)
-      .json({ message: "Data added to the JSON file successfully." });
-  } catch (error) {
-    console.error("Error processing the request:", error);
-    res.status(400).json({ error: "Something went wrong." });
-  }
-});
+      // ===== KILLS =====
+
+      // Loop through the array of objects and sum up the kills for each puuid
+      leaderboardData.forEach((entry) => {
+        const puuid = entry.puuid;
+        const kills = parseInt(entry.kills);
+        const name = entry.userName;
+
+        // If the puuid is already in the map, add the kills to its total
+        if (puuidKillsMap.hasOwnProperty(puuid)) {
+          puuidKillsMap[puuid].kills += kills;
+        } else {
+          // If the puuid is not in the map, initialize it with the current kills and name
+          puuidKillsMap[puuid] = { name, kills };
+        }
+      });
+
+      // Find the puuid with the highest total kills
+      let highestKills = 0;
+      let puuidWithHighestKills = null;
+      let nameWithHighestKills = null;
+
+      for (const puuid in puuidKillsMap) {
+        if (puuidKillsMap[puuid].kills > highestKills) {
+          highestKills = puuidKillsMap[puuid].kills;
+          puuidWithHighestKills = puuid;
+          nameWithHighestKills = puuidKillsMap[puuid].name;
+        }
+      }
+
+      const mostKills = {
+        puuid: puuidWithHighestKills,
+        name: nameWithHighestKills,
+        kills: highestKills,
+      };
+
+      // ===== DEATHS =====
+
+      leaderboardData.forEach((entry) => {
+        const puuid = entry.puuid;
+        const deaths = parseInt(entry.deaths);
+        const name = entry.userName;
+
+        // If the puuid is already in the map, add the kills to its total
+        if (puuidDeathsMap.hasOwnProperty(puuid)) {
+          puuidDeathsMap[puuid].deaths += deaths;
+        } else {
+          // If the puuid is not in the map, initialize it with the current kills and name
+          puuidDeathsMap[puuid] = { name, deaths };
+        }
+      });
+
+      let highestDeaths = 0;
+      let puuidWithHighestDeaths = null;
+      let nameWithHighestDeaths = null;
+
+      for (const puuid in puuidDeathsMap) {
+        if (puuidDeathsMap[puuid].deaths > highestDeaths) {
+          highestDeaths = puuidDeathsMap[puuid].deaths;
+          puuidWithHighestDeaths = puuid;
+          nameWithHighestDeaths = puuidDeathsMap[puuid].name;
+        }
+      }
+
+      const mostDeaths = {
+        puuid: puuidWithHighestDeaths,
+        name: nameWithHighestDeaths,
+        deaths: highestDeaths,
+      };
+
+      // ===== ASSISTS =====
+
+      leaderboardData.forEach((entry) => {
+        const puuid = entry.puuid;
+        const assists = parseInt(entry.assists);
+        const name = entry.userName;
+
+        if (puuidAssistsMap.hasOwnProperty(puuid)) {
+          puuidAssistsMap[puuid].assists += assists;
+        } else {
+          puuidAssistsMap[puuid] = { name, assists };
+        }
+      });
+
+      let highestAssists = 0;
+      let puuidWithHighestAssists = null;
+      let nameWithHighestAssists = null;
+
+      for (const puuid in puuidAssistsMap) {
+        if (puuidAssistsMap[puuid].assists > highestAssists) {
+          highestAssists = puuidAssistsMap[puuid].assists;
+          puuidWithHighestAssists = puuid;
+          nameWithHighestAssists = puuidAssistsMap[puuid].name;
+        }
+      }
+
+      const mostAssists = {
+        puuid: puuidWithHighestAssists,
+        name: nameWithHighestAssists,
+        assists: highestAssists,
+      };
+
+      // ===== KDA =====
+
+      // Find the puuid with the highest average KDA
+      let highestAverageKda = 0;
+      let puuidWithHighestAverageKda = null;
+      let nameWithHighestAverageKda = null;
+
+      leaderboardData.forEach((entry) => {
+        const puuid = entry.puuid;
+        const kda = parseFloat(entry.kda); // Convert to a floating-point number
+        const name = entry.userName;
+
+        if (kda > highestAverageKda) {
+          highestAverageKda = kda;
+          puuidWithHighestAverageKda = puuid;
+          nameWithHighestAverageKda = name;
+        }
+      });
+
+      const bestKda = {
+        puuid: puuidWithHighestAverageKda,
+        name: nameWithHighestAverageKda,
+        kda: highestAverageKda,
+      };
+
+      // ===== OUTPUT =====
+
+      const leaderboard = [
+        { mostKills },
+        { mostDeaths },
+        { mostAssists },
+        { bestKda },
+      ];
+
+      res.json(leaderboard);
+    });
+  });
 
 router.route("*").get((req, res) => {
   res.send(
